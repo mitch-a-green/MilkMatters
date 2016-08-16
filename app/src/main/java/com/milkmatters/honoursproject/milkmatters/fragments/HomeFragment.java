80c93e512e2c7e24ab7082991c09ca6a407c0d29
mitchell <mitch.a.green@gmail.com>
2016-08-16 15:40:35 +0200

    Initial commit

---
+++ app/src/main/java/com/milkmatters/honoursproject/milkmatters/fragments/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.milkmatters.honoursproject.milkmatters.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.milkmatters.honoursproject.milkmatters.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+    private View view;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment HomeFragment.
+     */
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+}
