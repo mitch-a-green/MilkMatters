86a1d9d3a0cbc5d10b3ce78feda3c77faf2e3727
mitchell <mitch.a.green@gmail.com>
2016-09-07 14:25:32 +0200

    - Added functionality to send an email automatically from the about screen

--- app/src/main/java/com/milkmatters/honoursproject/milkmatters/fragments/AboutFragment.java
+++ app/src/main/java/com/milkmatters/honoursproject/milkmatters/fragments/AboutFragment.java
@@ -1,12 +1,15 @@
 package com.milkmatters.honoursproject.milkmatters.fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.milkmatters.honoursproject.milkmatters.R;
 
@@ -32,18 +35,40 @@ public class AboutFragment extends Fragment {
         return fragment;
     }
 
+    /**
+     * Overridden onCreate method
+     * @param savedInstanceState the saved instance state
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Overridden onCreateView method
+     * @param inflater the layout inflater
+     * @param container the container
+     * @param savedInstanceState the saved instance state
+     * @return the created view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         this.view = inflater.inflate(R.layout.fragment_about, container, false);
 
-        hideFAB();
+        hideFAB(); // Hide the floating action button
+
+        // add functionality to send an automated email
+        Button emailUsButton = (Button) this.view.findViewById(R.id.email_us);
+        emailUsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String[] addresses = {"mitch.a.green@gmail.com", "chelseajoyful@gmail.com"};
+                String message = "Hi,\n\nI read about you in the Milk Matters app. I am " +
+                        "interested in finding out more...";
+                emailUs(addresses, "Want to Know More", message);
+            }
+        });
 
         return this.view;
     }
@@ -64,5 +89,20 @@ public class AboutFragment extends Fragment {
     {
         FloatingActionButton fab = (FloatingActionButton) this.getActivity().findViewById(R.id.fab);
         fab.hide();
+    }
+
+    /**
+     * Method to send an email to Milk Matters.
+     * @param addresses the email addresses
+     * @param subject the subject of the email
+     */
+    public void emailUs(String[] addresses, String subject, String message) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+        if (emailIntent.resolveActivity(getActivity().getPackageManager()) != null)
+            startActivity(emailIntent);
     }
 }
